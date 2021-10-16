b09ace24da58cbd79de674980387ab8077b43878
KrishnaCD93 <krishna.duvvuri93@gmail.com>
2021-10-15 21:03:27 -0600

    finish section 4 part 1

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,10 +6,12 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
-    // Create wave event
+    uint256 private seed; // Use this to help generate a random number.
+
+    // Create wave event.
     event NewWave(address indexed from, uint256 timestamp, string dish, string recipe);
 
-    // Create a struct name wave that allows us to customize what's inside
+    // Create a struct name wave that allows us to customize what's inside.
     struct Wave {
         address waver; // The address of the user who waved.
         string dish; // The dish the user sent.
@@ -20,14 +22,46 @@ contract WavePortal {
     //Declare a variable waves taht lets me store an array of structs.
     Wave[] waves;
 
-    constructor() {
+    // Address to uint mapping. Store the address of the last time the user waved.
+    mapping(address => uint256) public lastWavedAt;
+
+    constructor() payable{
         console.log("Ready to build a web3 dApp!");
     }
 
     function wave(string memory _dish, string memory _recipe) public {
+        // Make sure the current timestamp is at least 15 minutes bigger than the last timestamp.
+        require(
+            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15 min"
+        );
+        // Update the current timestamp for the user.
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
-        waves.push(Wave(msg.sender, _dish, _recipe, block.timestamp)); // Store the wave data in the array
+
+        waves.push(Wave(msg.sender, _dish, _recipe, block.timestamp)); // Store the wave data in the array.
+
+        // Generate a psuedo random number between 0 and 100.
+        uint256 randomNumber = (block.difficulty + block.timestamp + seed) % 100;
+        console.log("Random # generated: %s", randomNumber);
+
+        // Set the generated, random number as the seed for the next wave.
+        seed = randomNumber;
+
+        // Give a 50% chance that user wins the prize.
+        if (randomNumber < 50) {
+            console.log("%s won", msg.sender);
+        
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
         emit NewWave(msg.sender, block.timestamp, _dish, _recipe);
     }
 
--- scripts/run.js
+++ scripts/run.js
@@ -2,20 +2,36 @@ require("@nomiclabs/hardhat-waffle");
 
 const main = async () => {
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther('0.1'),
+    });
     await waveContract.deployed();
     console.log("Contract deployed to:", waveContract.address);
+
+    // Get Contract balance
+    let contractBalance = await hre.ethers.provider.getBalance(
+        waveContract.address
+    );
+    console.log('Contract balance:', hre.ethers.utils.formatEther(contractBalance))
     
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
     console.log(waveCount.toNumber());
 
+    // Send a wave
     let waveTxn = await waveContract.wave('Pasta', 'One-Pot Vegetarian Pasta with Alfredo sauce');
     await waveTxn.wait(); // Wait for transaction to be mined
 
+    // Another wave
     const [_, randomPerson] = await hre.ethers.getSigners();
-    waveTxn = await waveContract.connect(randomPerson).wave('Pizza', 'Vegetarian with spinach and mushrooms');
+    waveTxn = await waveContract.wave('Pizza', 'Vegetarian with spinach and mushrooms');
     await waveTxn.wait(); // Wait for transaction to be mined
+
+    //Get contract balance to see what happened
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+        'Contract balance:', hre.ethers.utils.formatEther(contractBalance)
+    );
 
     let allWaves = await waveContract.getAllWaves();
     console.log(allWaves);
